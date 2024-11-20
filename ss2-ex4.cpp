86cef06799b47ea9e7701b9bdab28d443a4575ca
vthang0412 <vthang041206@gmail.com>
2024-11-21 01:20:56 +0700

    Add files via upload

---
+++ ss2-ex4.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main (){
+	int edge=5 , chuvi, dientich;
+	chuvi =edge * 6;
+	dientich=edge  * edge;
+	printf ("chu vi hinh vuong la : %d\n ",chuvi) ;
+	printf ("dien tich hinh vuong la : %d\n ",dientich) ;
+	return 0; 
+} 
