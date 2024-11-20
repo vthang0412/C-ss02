86191ba0719c8a7e99bf95d52d30393c11f3d372
vthang0412 <vthang041206@gmail.com>
2024-11-21 01:09:55 +0700

    Add files via upload

---
+++ ss2-ex3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int number = 9;
+	int secondNumber  = 9;
+	int Tong = number + secondNumber ;
+	int Hieu = number - secondNumber ;
+	int Nhan = number * secondNumber ;
+	int Thuong = number / secondNumber ;
+	printf ("Tong cua %d va %d la: %d\n",number,secondNumber,Tong);
+	printf ("Hieu cua %d va %d la: %d\n",number,secondNumber,Hieu);
+	printf ("Nhan cua %d va %d la: %d\n",number,secondNumber,Nhan);
+	printf ("Thuong cua %d va %d la: %d\n",number,secondNumber,Thuong);
+    return 0;
+}
