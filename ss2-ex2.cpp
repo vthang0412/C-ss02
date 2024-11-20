daee0618e84817c538d0e2a060ae63ff1688b0d5
vthang0412 <vthang041206@gmail.com>
2024-11-21 01:09:26 +0700

    Update ss2-ex2.cpp

--- ss2-ex2.cpp
+++ ss2-ex2.cpp
@@ -4,9 +4,6 @@ int main() {
     int number = 9;
 	int secondNumber  = 9;
 	int Tong = number + secondNumber ;
-	
 	printf ("Tong cua %d va %d la: %d\n",number,secondNumber,Tong);
-	
-	
-    return 0;
+	return 0;
 }
