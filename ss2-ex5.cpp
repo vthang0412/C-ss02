1d99afdf5cf77d9ea3e46b0144dee2d3c410e505
vthang0412 <vthang041206@gmail.com>
2024-11-21 01:25:39 +0700

    Add files via upload

---
+++ ss2-ex5.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main (){
+	int chieudai=5 ,chieurong=4, chuvi, dientich;
+	chuvi =(chieudai+chieurong)*2;
+	dientich=chieudai  * chieurong;
+	printf ("chu vi hinh chu nhat la : %d\n ",chuvi) ;
+	printf ("dien tich hinh chu nhat la : %d\n ",dientich) ;
+	return 0; 
+} 
