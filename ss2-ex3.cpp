f0556052d537b83eb8ddeb7358e6fbaa3357f71e
vthang0412 <vthang041206@gmail.com>
2024-11-21 01:46:51 +0700

    Update ss2-ex3.cpp

--- ss2-ex3.cpp
+++ ss2-ex3.cpp
@@ -2,14 +2,14 @@
 
 int main() {
     int number = 9;
-	int secondNumber  = 9;
+	int secondNumber  = 10;
 	int Tong = number + secondNumber ;
 	int Hieu = number - secondNumber ;
 	int Nhan = number * secondNumber ;
-	int Thuong = number / secondNumber ;
+	double Thuong = (double)number / secondNumber ;
 	printf ("Tong cua %d va %d la: %d\n",number,secondNumber,Tong);
 	printf ("Hieu cua %d va %d la: %d\n",number,secondNumber,Hieu);
 	printf ("Nhan cua %d va %d la: %d\n",number,secondNumber,Nhan);
-	printf ("Thuong cua %d va %d la: %d\n",number,secondNumber,Thuong);
+	printf ("Thuong cua %d va %d la: %.2lf\n",number,secondNumber,Thuong);
     return 0;
 }
